c07c786d7e1bcb9040deb1b965e7066d7b5a8d26
ANGEL SANTIAGO GUZMAN JARAMILLO <anguja@alumnatflorida.es>
2025-11-28 11:12:50 +0100

    mitad del ejercicio 4

---
+++ ej4.php
@@ -0,0 +1,26 @@
+ <?php
+class coche {
+    private $marca;
+    private $modelo;
+    function __construct($marca="no tienes uno asignado",$modelo="no tienes uno asignado"){
+        $this->marca=$marca;
+        $this->modelo=$modelo;
+    }
+    public function getmarca(){
+        return $this->marca;
+    }
+    public function getmodelo(){
+        return $this->modelo;
+    }
+}
+$coche1=new coche("toyota","corola");
+echo $coche1->getmarca() .$coche1->getmodelo();
+class cuentaBancaria {
+    public $titula;
+    public $saldo;
+    public function __construct($titula="debe de colocar un titular",$saldo="0"){
+        $this->titula=$titula;
+        $this->saldo=$saldo;
+    }
+    
+}
