18f111a25bfa4f45736cb7d76fedde4c2fa6ffea
ANGEL SANTIAGO GUZMAN JARAMILLO <anguja@alumnatflorida.es>
2025-11-28 09:44:51 +0100

    35 de el ej2

--- ej2.php
+++ ej2.php
@@ -1,1 +1,19 @@
 <?php 
+<?php
+class coche {
+    private $marca;
+    private $modelo;
+    public function getMarca(){
+        return $this->marca;
+    public function setMarca(){
+        
+    }
+    }
+}
+function mostrarInfo(){
+    $coche1= new coche();
+    $marca1=$coche1->marca="twingo";
+    $modelo1=$coche1->modelo="pique";
+    echo "$marca1,$modelo1";
+}
+echo mostrarInfo();
