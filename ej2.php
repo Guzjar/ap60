c6f318d8aed3b16d0a9aa47737e2255b002853ea
ANGEL SANTIAGO GUZMAN JARAMILLO <anguja@alumnatflorida.es>
2025-11-28 10:24:03 +0100

    finalizado ejercici 2

--- ej2.php
+++ ej2.php
@@ -1,19 +1,22 @@
-<?php 
-<?php
+ <?php
 class coche {
     private $marca;
     private $modelo;
     public function getMarca(){
         return $this->marca;
-    public function setMarca(){
-        
+         }
+    public function setMarca($marca){
+        $this->marca= $marca;
     }
+    public function getmodelo(){
+        return $this->modelo;
+    }
+    public function setmodelo($modelo){
+        $this ->modelo= $modelo;
     }
 }
-function mostrarInfo(){
-    $coche1= new coche();
-    $marca1=$coche1->marca="twingo";
-    $modelo1=$coche1->modelo="pique";
-    echo "$marca1,$modelo1";
-}
-echo mostrarInfo();
+$coche1=new coche();
+$coche1->setmodelo('pique');
+$coche1->setMarca('twingo');
+echo $coche1->getMarca().",".$coche1->getmodelo();
+ 
